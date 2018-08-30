d3be1037110ce59aba8adb238a7fb7b68e92aefc
Vetal <mou.mail.com@gmil.com>
2018-08-30 15:57:14 +0300

    no header

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
 
 var columnDefs = [
-    {headerName: "Gold", field: "gold", filter: 'agNumberColumnFilter', suppressMenu:true,
-        floatingFilterComponent:'customNumberFloatingFilter',
-        floatingFilterComponentParams:{
-            suppressFilterButton:true,
-            color:'red'
-        }
+    {
+        headerName: "Gold", 
+        field: "gold",
+        filter: 'agTextColumnFilter', 
+        suppressMenu: true,
+        // suppressColumnVirtualisation: true,
+        // suppressResize: true,
+        // suppressSizeToFit: true,
+        floatingFilterComponent: 'customNumberFloatingFilter',
+        floatingFilterComponentParams: {
+            suppressFilterButton: true,
+            // color: 'red'
+            
+        },
+        // width: "500",
+    
+        headerClass: 'my-css-class',
+
+     
     },
 ];
 
 var gridOptions = {
-    components:{
+    components: {
+        headerHeight: 1500,
         customNumberFloatingFilter: getNumberFloatingFilterComponent()
     },
-    floatingFilter:true,
+    floatingFilter: true,
     columnDefs: columnDefs,
-
+    // enableColResize: true,
     rowData: null,
-    enableFilter: true
+    headerHeight: 0,
+    // enableFilter: true,
+    // suppressColumnVirtualisation: true,
+    // colSpan: 51
+
+    headerCellRenderer: (params) =>{return '<h1 column="headerColDef">' + 111 + '</h1>'}
 };
 
-function getNumberFloatingFilterComponent (){
+function getNumberFloatingFilterComponent() {
     function NumberFloatingFilter() {
     }
 
     NumberFloatingFilter.prototype.init = function (params) {
+        // this.suppressResize = true;
         this.onFloatingFilterChanged = params.onFloatingFilterChanged;
         this.eGui = document.createElement('div');
         this.eGui.innerHTML = '<input style="width:100%" type="text"/>'
         this.currentValue = null;
         this.eFilterInput = this.eGui.querySelector('input');
-    this.eFilterInput.style.color = params.color;
+        // this.eFilterInput.style.color = params.color;
         var that = this;
-        function onInputBoxChanged(){
+        function onInputBoxChanged() {
             if (that.eFilterInput.value === '') {
                 //Remove the filter
                 that.onFloatingFilterChanged(null);
                 return;
             }
 
-            that.currentValue = Number(that.eFilterInput.value);
-        that.onFloatingFilterChanged({model:{
-            //In this example we are only interested in filtering by greaterThan
-            type:'equals',
-            filter:that.currentValue
-        }});
+            that.currentValue = String(that.eFilterInput.value);
+            that.onFloatingFilterChanged({
+                model: {
+                    //In this example we are only interested in filtering by greaterThan
+                    type: 'equals',
+                    filter: that.currentValue
+                }
+            });
         }
         this.eFilterInput.addEventListener('input', onInputBoxChanged);
     };
@@ -53,10 +75,10 @@ function getNumberFloatingFilterComponent (){
         // When the filter is empty we will receive a null message her
         if (!parentModel) {
             this.eFilterInput.value = '';
-        this.currentValue = null;
+            this.currentValue = null;
         } else {
-        this.eFilterInput.value = parentModel.filter + '';
-        this.currentValue = parentModel.filter;
+            this.eFilterInput.value = parentModel.filter + '';
+            this.currentValue = parentModel.filter;
         }
     };
 
@@ -70,7 +92,7 @@ function getNumberFloatingFilterComponent (){
 
 
 // setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 
@@ -79,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', 'https://raw.githubusercontent.com/ag-grid/ag-grid-docs/master/src/olympicWinnersSmall.json');
     httpRequest.send();
-    httpRequest.onreadystatechange = function() {
+    httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
             var httpResult = JSON.parse(httpRequest.responseText);
             gridOptions.api.setRowData(httpResult);
